127e819f52cd773c11f6f15430f6ccdfcc128e6e
DanielyanAndranik <danielyandranik@gmail.com>
2018-05-20 00:42:50 +0400

    Initial commit

---
+++ ClientServerApplication/MathClient/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose which service do you want to use: TCP or UDP. TCP = 1, UDP = 2");
+            var service = Console.ReadLine();
+            // Server Endpoint
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.105"), 4000);
+            //local endpoint.
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 7888);
+
+            // Creates a Tcp client.
+            if (service == "1")
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(remoteEndPoint);
+                var stream = client.GetStream();
+
+                Console.WriteLine("Please enter the math operation you want to do. Operation format is above. Or enter 'finish' to finish.");
+                Console.WriteLine("operator: first_value:second_value");
+               
+                while (true)
+                {
+                    var data = new StringBuilder();
+                    string operation = Console.ReadLine();
+                    byte[] buffer = Encoding.ASCII.GetBytes(operation);
+                    stream.Write(buffer, 0, buffer.Length);
+                    if(operation == "finish")
+                    {
+                        client.Close();
+                        break;
+                    }
+
+                    var i = stream.Read(buffer, 0, buffer.Length);
+
+                    while (i != 0)
+                    {
+                        data.Append(Encoding.ASCII.GetString(buffer, 0, i));
+                        if (i < buffer.Length)
+                        {
+                            break;
+                        }
+                        if(!stream.DataAvailable)
+                        {
+                            break;
+                        }
+                        i = stream.Read(buffer, 0, buffer.Length);
+
+                    }
+                    Console.WriteLine(data);
+                }    
+            }
+            // Creates a Udp client.
+            else if(service == "2")
+            {
+                UdpClient client = new UdpClient(localEndPoint);
+                client.Connect(remoteEndPoint);
+                Console.WriteLine("Please enter the math operation you want to do. Operation format is above. Or enter 'finish' to finish.");
+                Console.WriteLine("operator: first_value:second_value");
+                while(true)
+                {
+                    //var data = new StringBuilder();
+                    string operation = Console.ReadLine();
+                    byte[] buffer = Encoding.ASCII.GetBytes(operation);
+                    client.Send(buffer, buffer.Length);
+                    if(operation == "finish")
+                    {
+                        client.Close();
+                    }
+
+                    var bytes = client.Receive(ref remoteEndPoint);
+                    Console.WriteLine(Encoding.ASCII.GetString(bytes));
+                    if(Encoding.ASCII.GetString(bytes) == "bye")
+                        break;
+                }
+                client.Close();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
---
+++ ClientServerApplication/MathServer/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Server local endpoint.
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 4000);
+
+            // Creates a new task to manage tcp clients.
+            var tcpListenerTask = new Task(() =>
+            {
+                TcpListener tcpListener = new TcpListener(localEndPoint);
+
+                tcpListener.Start();
+
+                // Accepts as more clients as they connect.
+                while (true)
+                {
+                    var tcpClient = tcpListener.AcceptTcpClient();
+
+                    Console.WriteLine("Connection accepted from " + tcpClient.Client.RemoteEndPoint);
+
+                    // Creates a new task for each client.
+                    var tcpTask = new Task((c) =>
+                    {
+                        var client = (TcpClient)c;
+
+                        var stream = client.GetStream();
+
+                        byte[] buffer = new byte[1024];
+
+                        while (true)
+                        {
+                            var data = new StringBuilder();
+                            var i = stream.Read(buffer, 0, buffer.Length);
+
+                            while (i != 0)
+                            {
+                                data.Append(Encoding.ASCII.GetString(buffer, 0, i));
+                                if (i < buffer.Length)
+                                {
+                                    break;
+                                }
+                                if(!stream.DataAvailable)
+                                {
+                                    break;
+                                }
+                                i = stream.Read(buffer, 0, buffer.Length);
+                            }
+                            Console.WriteLine(data);
+
+                            if (data.ToString() == "finish")
+                            {                              
+                                tcpClient.Close();
+                                break;
+                            }
+
+                            double? result;
+
+                            string answer = null;
+
+                            if (!TryDoMathOperation(data.ToString(), out result))
+                            {
+                                answer = "Invalid operation.";
+                            }
+                            else
+                            {
+                                answer = result.ToString();
+                            }
+
+                            stream.Write(Encoding.ASCII.GetBytes(answer), 0, Encoding.ASCII.GetBytes(answer).Length);
+
+                        }
+
+                    }, tcpClient);
+
+                    tcpTask.Start();                    
+                }
+                tcpListener.Stop();
+            });
+
+            tcpListenerTask.Start();
+
+            // Creates a Udp client.
+            UdpClient udpClient = new UdpClient(localEndPoint);
+
+            // Creates a task to manage  clients.
+            var udpTask = new Task(() =>
+            {
+               
+                while (true)
+                {
+                    var endPoint = new IPEndPoint(IPAddress.Any, 0);
+                    var buffer = udpClient.Receive(ref endPoint);
+                    Console.WriteLine("Connection accepted from " + endPoint.Address.Address);
+                    // Creates a task for each client.
+                    var clientTask = new Task((udpData) =>
+                    {
+                        var data = (UdpData)udpData;
+                        while(true)
+                        {
+                            var request = Encoding.ASCII.GetString(data.Buffer);
+                            Console.WriteLine(request);
+
+                            if(request == "finish")
+                            {
+                                break;
+                            }
+
+                            double? result;
+
+                            string answer = null;
+
+                            if (!TryDoMathOperation(request.ToString(), out result))
+                            {
+                                answer = "Invalid format.";
+                            }
+                            else
+                            {
+                                answer = result.ToString();
+                            }
+
+                            udpClient.Send(Encoding.ASCII.GetBytes(answer), Encoding.ASCII.GetBytes(answer).Length, data.EndPoint);
+                            buffer = udpClient.Receive(ref endPoint);
+                        }                       
+
+                    }, new UdpData { EndPoint = endPoint, Buffer = buffer});
+
+                    clientTask.Start();
+                }
+
+                udpClient.Close();
+            });
+
+            udpTask.Start();
+
+            Task.WaitAll(tcpListenerTask, udpTask);
+        }
+
+        /// <summary>
+        /// Basic struct for passing as argument to the udp task.
+        /// </summary>
+        private struct UdpData
+        {
+            public IPEndPoint EndPoint { get; set; }
+            public byte[] Buffer { get; set; }
+        }
+
+        /// <summary>
+        /// Makes basic math operations.
+        /// </summary>
+        /// <param name="operation">Operation with a specified format.</param>
+        /// <param name="result">The result of operation.</param>
+        /// <returns>Returns true if operation was successfully done, else returns false.</returns>
+        private static bool TryDoMathOperation(string operation, out double? result)
+        {
+            result = null;
+            var splitString = operation.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+            if (splitString.Length != 3) return false;
+            var oper = splitString[0];
+
+            double firstOperand;          
+            if (!Double.TryParse(splitString[1], out firstOperand)) return false;
+            double secondOperand;
+            if (!Double.TryParse(splitString[2], out secondOperand)) return false;
+
+            MathService service = new MathService();
+
+            Operations operations = null;
+
+            switch(oper)
+            {
+                case "+" :
+                    operations += service.Add;
+                    break;
+                case "-":
+                    operations += service.Sub;
+                    break;
+                case "*":
+                    operations += service.Mult;
+                    break;
+                case "/":
+                    if (secondOperand == 0) return false;
+                    operations += service.Div;
+                    break;
+                default:
+                    return false;
+            }
+
+            result = operations(firstOperand, secondOperand);
+            return true;
+        }
+    }
+
+    delegate double Operations(double x, double y);
+}
